730c7c0ce56c7c714f4c05938fde922517f9b78f
Chris Wheeler <>
2021-04-30 17:40:22 +0000

    Add the roll command

--- commands.py
+++ commands.py
@@ -17,8 +17,8 @@ async def ping(context):
 	await context.send(f'Pong! It took {round(bot.latency, 2)} ms.')
 
 
-@bot.command(name='py')
-async def calc(context, *, string):
+@bot.command()
+async def py(context, *, string):
 	'''Evaluate a Python expression and return the output.
 	
 	Print statements don't work with this because they don't return anything.
@@ -32,14 +32,25 @@ async def calc(context, *, string):
 @bot.command()
 async def rand(context, low=1, high=6):
 	'''Get a random number. Default bounds are 1 and 6.'''
-	
 	low = int(low)
 	high = int(high)
-
 	if  low <= high:
 		await context.send(str(random.randint(low, high)))
 	else:
 		await context.send(str(random.randint(high, low)))
+
+
+@bot.command()
+async def roll(context, dice: str):
+    """Roll dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await context.send('Error: format has to be in NdN.')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await context.send(result)
 
 
 @bot.command(name='flip-coin')
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # https://ritza.co/showcase/repl.it/building-a-discord-bot-with-python-and-repl-it.html
 # https://www.freecodecamp.org/news/create-a-discord-bot-with-python/
 # https://discordpy.readthedocs.io/en/latest/ext/commands/commands.html#invocation-context
+# https://github.com/Rapptz/discord.py/tree/v1.7.1/examples
 
 import os
 import discord
