2fffb782c361505049982ebd1c52e4fc02245747
Chris Wheeler <39012918+wheelercj@users.noreply.github.com>
2021-06-11 01:45:36 +0000

    With typing in math

--- cogs/other.py
+++ cogs/other.py
@@ -134,18 +134,19 @@ class Other(commands.Cog):
             expressions_json = '{\n"expr": ' + expressions + '\n}'
 
             async with aiohttp.ClientSession() as cs:
-                async with cs.post('http://api.mathjs.org/v4/',
-                    data = expressions_json,
-                    headers = {'content-type': 'application/json'},
-                    timeout = 10
-                ) as response:
-                    if not response and response.status != 400:
-                        raise ValueError(f'API request failed with status code {response.status}.')
+                async with ctx.typing():
+                    async with cs.post('http://api.mathjs.org/v4/',
+                        data = expressions_json,
+                        headers = {'content-type': 'application/json'},
+                        timeout = 10
+                    ) as response:
+                        if not response and response.status != 400:
+                            raise ValueError(f'API request failed with status code {response.status}.')
 
-                    json_text = await response.json()
+                        json_text = await response.json()
 
-                    if response.status == 400:
-                        raise ValueError(json_text['error'])
+                        if response.status == 400:
+                            raise ValueError(json_text['error'])
 
             result = ''
             for i, expr in enumerate(raw_expressions):
