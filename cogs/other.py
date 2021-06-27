84e054ed8d9df4891f7b0d5f61d118551b5544a8
wheelercj <chrisw289@gmail.com>
2021-06-27 11:43:04 -0700

    Create privacy-policy command

--- cogs/other.py
+++ cogs/other.py
@@ -6,6 +6,7 @@ from datetime import datetime, timezone
 import json
 import discord
 from discord.ext import commands
+from textwrap import dedent
 
 # Internal imports
 from common import remove_backticks, send_traceback, get_prefixes_str, dev_settings
@@ -63,6 +64,40 @@ class Other(commands.Cog):
         '''Shows the current time in UTC'''
         current_time = datetime.now(timezone.utc)
         await ctx.send(f'The current time is {current_time} UTC')
+
+
+    @commands.command(name='privacy-policy', aliases=['privacy'])
+    @commands.cooldown(1, 15, commands.BucketType.user)
+    async def privacy_policy(self, ctx):
+        '''Shows the bot\'s privacy policy'''
+        embed = discord.Embed()
+        embed.add_field(name='Privacy Policy',
+            value=dedent('''
+                Last updated: June 27, 2021
+                This Privacy Policy may be updated at any time without any notification.
+
+                **agreement**
+                By using this bot or by being in a Discord channel that this bot can see, you agree to the following Privacy Policy.
+
+                **what data is stored**
+                * If you create reminder(s) or a daily quote, the bot will store your Discord user ID, the server ID (if applicable), the channel ID (if applicable), and any text input you give the bot.
+                * If you send a message in a channel that the bot can see, some data may be temporarily logged and/or cached including but not limited to: the message and your Discord user ID, name, and discriminator. For more details, click [here](https://discordpy.readthedocs.io/en/latest/ext/commands/api.html?highlight=context#discord.ext.commands.Context).
+
+                **why data is stored**
+                Data is only stored and used to allow the bot to function properly, to fix bugs, and to investigate errors. Data is retained as long as needed to fulfill these purposes.
+
+                **who has access to your data**
+                Your data is not shared with any third parties.
+
+                **questions and concerns**
+                Please join the [support server](https://discord.gg/mCqGhPJVcN).
+
+                **how to delete your data**
+                You can use the `del-r` and `quote stop` commands, or join the [support server](https://discord.gg/mCqGhPJVcN) and ask about having your data deleted.
+            ''')
+        )
+
+        await ctx.send(embed=embed)
 
 
     @commands.command(name='server-info', aliases=['serverinfo'])
