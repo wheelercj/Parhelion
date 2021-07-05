7d270af29fec96fc9afb2ac9e0658f1178e4537f
Chris Wheeler <39012918+wheelercj@users.noreply.github.com>
2021-07-05 19:15:17 +0000

    Require confirmation when bulk-deleting messages

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,5 +1,8 @@
+# external imports
 from discord.ext import commands
 
+# internal imports
+from common import get_display_prefixes
 
 class Admin(commands.Cog):
     def __init__(self, bot):
@@ -14,11 +17,17 @@ class Admin(commands.Cog):
 
     @commands.command(name='delete', aliases=['clear', 'cleanup'])
     @commands.bot_has_guild_permissions(read_message_history=True, manage_messages=True)
-    async def _delete(self, ctx, n: int):
+    async def _delete(self, ctx, n: int, confirm: str = None):
         """Deletes the previous n messages, up to 100
-        
+
+        This cannot be undone or stopped once it begins.
         You cannot delete messages more than 14 days old.
         """
+        if confirm != 'CONFIRM':
+            p = await get_display_prefixes(self.bot, ctx.message)
+            await ctx.send(f'Are you sure you want to bulk-delete the last {n} messages? This cannot be undone or stopped once it begins. Use `{p[0]}delete {n} CONFIRM` to confirm.')
+            return
+
         if n > 100:
             await ctx.send('You can delete up to 100 messages at once.')
             return
