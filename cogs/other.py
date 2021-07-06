42900c45f40b2a369666b1580ffb007eb939762c
Chris Wheeler <39012918+wheelercj@users.noreply.github.com>
2021-07-06 18:17:59 +0000

    Put a longer cooldown on the math command

--- cogs/other.py
+++ cogs/other.py
@@ -15,12 +15,15 @@ class Other(commands.Cog):
 
 
     @commands.command(aliases=['calc', 'solve', 'maths'])
+    @commands.cooldown(1, 30, commands.BucketType.user)
     async def math(self, ctx, *, expression: str):
         """Evaluates a math expression
         
         Evaluates multiple expressions if they're on separate lines, and supports code blocks.
         Uses the math.js API: https://mathjs.org/docs/expressions/syntax.html
         """
+        # The math.js API allows a maximum of 10,000 requests per
+        # day with a 10 second duration per evaluation.
         try:
             expression = remove_backticks(expression)
             if '**' in expression:
