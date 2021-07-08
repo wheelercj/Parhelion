9199922160dd84b1ed342457ae7fd54df1165e2c
Chris Wheeler <39012918+wheelercj@users.noreply.github.com>
2021-07-08 07:19:04 +0000

    Delete redundant commands
    These commands are also in Jishaku.

--- cogs/owner.py
+++ cogs/owner.py
@@ -1,8 +1,6 @@
 # external imports
 import os
 from discord.ext import commands
-import textwrap
-import inspect
 import asyncio
 import aiohttp
 from textwrap import dedent
@@ -27,12 +25,6 @@ class Owner(commands.Cog):
     async def echo(self, ctx, *, message: str):
         """Repeats a message"""
         await ctx.send(message)
-
-
-    @commands.command(name='shut-down')
-    async def shut_down(self, ctx):
-        """Shuts down the bot"""
-        await self.bot.close()
 
 
     @commands.command()
@@ -104,146 +96,6 @@ class Owner(commands.Cog):
                         await asyncio.sleep(n * 1.5)
                 except IndexError:
                     pass
-
-
-    @commands.command(name='list-exts', aliases=['list-ext', 'list-extensions'])
-    async def list_extensions(self, ctx):
-        """Lists all currently loaded extensions"""
-        message = 'Currently loaded extensions:\n' \
-            + '\n'.join(self.bot.extensions.keys())
-        await ctx.send(message)
-
-
-    @commands.command(name='reload', aliases=['reload-ext', 'reload-exts'])
-    async def reload_extension(self, ctx, *, extension: str):
-        """Reloads an extension"""
-        extensions = extension.split()
-        message = ''
-        for ext in extensions:
-            try:
-                self.bot.unload_extension(ext)
-                self.bot.load_extension(ext)
-                message += f'\nExtension {ext} successfully reloaded.'
-            except Exception as e:
-                message += f'\nError: {type(e).__name__}: {e}'
-
-        await ctx.send(message)
-
-
-    @reload_extension.error
-    async def reload_extension_error(self, ctx, error):
-        message = ''
-        if isinstance(error, commands.errors.MissingRequiredArgument):
-            message += str(error) + '\nExtensions: '
-            message += ', '.join(self.bot.extensions.keys())
-
-            await ctx.send(message)
-        else:
-            await ctx.send(error)
-
-
-    @commands.command(name='load', aliases=['load-ext', 'load-exts'])
-    async def load_extension(self, ctx, *, extension: str):
-        """Loads an extension"""
-        extensions = extension.split()
-        message = ''
-        for ext in extensions:
-            try:
-                self.bot.load_extension(ext)
-                message += f'\nExtension {ext} successfully loaded.'
-            except Exception as e:
-                message += f'\nError: {type(e).__name__}: {e}'
-
-        await ctx.send(message)
-
-
-    @commands.command(name='unload', aliases=['unload-ext', 'unload-exts'])
-    async def unload_extension(self, ctx, *, extension: str):
-        """Unloads an extension"""
-        extensions = extension.split()
-        message = ''
-        for ext in extensions:
-            try:
-                self.bot.unload_extension(ext)
-                message += f'\nExtension {ext} successfully unloaded.'
-            except Exception as e:
-                message += f'\nError: {type(e).__name__}: {e}'
-
-        await ctx.send(message)
-
-
-    @commands.command(name='reload-all', aliases=['reload-all-exts'])
-    async def reload_all_extensions(self, ctx):
-        """Reloads all currently loaded extensions"""
-        message = ''
-        extensions = list(self.bot.extensions.keys())
-        for ext in extensions.copy():
-            try:
-                self.bot.unload_extension(ext)
-                self.bot.load_extension(ext)
-                message += f'\nExtension {ext} successfully reloaded.'
-            except Exception as e:
-                message += f'\nError: {type(e).__name__}: {e}'
-            
-        await ctx.send(message)
-
-
-    @commands.command(name='eval', aliases=['evaluate'])
-    async def _eval(self, ctx, *, expression: str):
-        """Evaluates a Python expression
-        
-        Returns result to Discord.
-        Has access to bot via self.
-        """
-        # This command must never be made available to anyone
-        # besides this bot's developers because Python's eval
-        # function is not safe.
-        try:
-            await ctx.send(eval(expression))
-        except Exception as e:
-            await ctx.send(f'Python error: {e}')
-
-
-    @commands.command(name='py', aliases=['python', 'exec', 'exe', 'execute'])
-    async def _exec(self, ctx, *, statement: str):
-        """Executes a Python statement
-        
-        Requires use of `await ctx.send` for output.
-        Has direct access to bot.
-        """
-        # This command must never be made available to anyone
-        # besides this bot's developers because Python's exec
-        # function is not safe.
-        _, statement = await unwrap_code_block(statement)
-        env = {
-            'ctx': ctx,
-            'bot': self.bot,
-            'asyncio': asyncio,
-        }
-
-        try:
-            code = f'async def func():\n    try:\n{textwrap.indent(statement, "        ")}\n    except Exception as e:\n        await ctx.send("Python error: %s" % e)\nasyncio.get_running_loop().create_task(func())'
-            
-            exec(code, env)
-            await ctx.message.add_reaction('✅')
-        except Exception as e:
-            await ctx.message.add_reaction('❗')
-            await ctx.send(f'Python error: {e}')
-
-
-    @commands.command(name='inspect', aliases=['source', 'src', 'getsource'])
-    async def _inspect(self, ctx, *, command: str):
-        """Shows the source code of a command"""
-        try:
-            cmds = {cmd.name: cmd for cmd in self.bot.commands}
-            if command not in cmds.keys():
-                raise ValueError(f'Command {command} not found.')
-            source = str(inspect.getsource(cmds[command].callback))
-            await ctx.send(f'```py\n{source}```')
-        except ValueError as e:
-            await ctx.send(e)
-        except KeyError as e:
-            await ctx.send(e)
 
 
 def setup(bot):
