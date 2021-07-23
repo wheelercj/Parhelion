451a4c6b9a72a55a38e20bccc92f5f1c592bfe83
Chris Wheeler <39012918+wheelercj@users.noreply.github.com>
2021-07-23 18:22:46 +0000

    Create access column for "allow", "deny", or "limit"

--- cogs/settings.py
+++ cogs/settings.py
@@ -2,17 +2,16 @@
 import discord
 from discord.ext import commands
 import asyncpg
-from typing import Optional
 
 
 '''
     CREATE TABLE IF NOT EXISTS command_access_settings (
         id SERIAL PRIMARY KEY,
         command_name TEXT,
-        is_blacklist BOOL DEFAULT TRUE,  -- else it's a whitelist
+        access TEXT CONSTRAINT valid_access CHECK (access = ANY('{"allow", "deny", "limit"}')),
         object_type TEXT CONSTRAINT valid_type CHECK (object_type = ANY('{"server", "channel", "user"}')),
         object_ids BIGINT[],
-        UNIQUE (command_name, is_blacklist, object_type)
+        UNIQUE (command_name, access, object_type)
     )
 '''
 # Each command may have multiple rows.
@@ -44,12 +43,9 @@ class Settings(commands.Cog):
 
         content = ''
         for r in records:
-            content += f'\n\nID: {r["id"]}\n'
-            if r['is_blacklist']:
-                content += 'blacklist of '
-            else:
-                content += 'whitelist of '
-            content += r['object_type'] + 's:'
+            content += f'\n\nID: {r["id"]}\n' \
+                + r['access'] + ' access to ' \
+                + r['object_type'] + 's:'
             for ID in r['object_ids']:
                 content += f'\n {ID}'
 
@@ -59,40 +55,35 @@ class Settings(commands.Cog):
 
 
     @setting.command(name='server', aliases=['s'])
-    async def server_cmd_access(self, ctx, server: Optional[discord.Guild], allow_or_deny: str, *, command_name: str):
+    async def server_cmd_access(self, ctx, server: discord.Guild, access: str, *, command_name: str):
         """Manages commands access for a server
 
-        If a server is not specified, the current server is used. The command name must not contain any aliases.
+        For the `access` argument, you may enter "allow", "deny", or "limit". The command name must not contain any aliases.
         """
-        if server is None:
-            server = ctx.guild
-            if server is None:
-                raise commands.BadArgument('You must specify a server or use this command in a server.')
-        allow_or_deny = allow_or_deny.strip('"')
-        if allow_or_deny not in ('allow', 'deny'):
-            raise commands.BadArgument('Please enter either "allow" or "deny" before the command that you are changing the settings of.')
+        access = access.strip('"')
+        if access not in ('allow', 'deny', 'limit'):
+            raise commands.BadArgument('Please enter either "allow", "deny", or "limit" before the command that you are changing the settings of.')
         all_command_names = [x.name for x in self.bot.commands]
         entered = command_name.split(' ')
         for command_name in entered:
             if command_name not in all_command_names:
                 raise commands.BadArgument(f'Command "{command_name}" not found.')
 
-        is_blacklist = allow_or_deny != 'allow'
         try:
             await self.bot.db.execute("""
                 INSERT INTO command_access_settings
-                (command_name, is_blacklist, object_type, object_ids)
+                (command_name, access, object_type, object_ids)
                 VALUES ($1, $2, 'server', $3);
-                """, command_name, is_blacklist, [server.id])
+                """, command_name, access, [server.id])
         except asyncpg.exceptions.UniqueViolationError:
             await self.bot.db.execute("""
                 UPDATE command_access_settings
                 SET object_ids = object_ids || $1
                 WHERE command_name = $2
-                    AND is_blacklist = $3
+                    AND access = $3
                     AND object_type = 'server'
                     AND $1 != ANY(object_ids);
-                """, server.id, command_name, is_blacklist)
+                """, server.id, command_name, access)
 
         await ctx.message.add_reaction('✅')
 
