66aede386651e8089a68b3fce4aa3e2f95f94870
Chris Wheeler <39012918+wheelercj@users.noreply.github.com>
2021-07-23 17:56:05 +0000

    Generalize server settings command

--- cogs/settings.py
+++ cogs/settings.py
@@ -2,6 +2,7 @@
 import discord
 from discord.ext import commands
 import asyncpg
+from typing import Optional
 
 
 '''
@@ -57,28 +58,41 @@ class Settings(commands.Cog):
         await ctx.send(embed=embed)
 
 
-    @setting.command(name='whitelist-server', aliases=['wls'])
-    async def whitelist_server(self, ctx, server: discord.Guild, *, command_name: str):
-        """Whitelists a server to use a command"""
-        command_names = [x.name for x in self.bot.commands]
-        if command_name not in command_names:
-            raise commands.BadArgument(f'Command "{command_name}" not found.')
+    @setting.command(name='server', aliases=['s'])
+    async def server_cmd_access(self, ctx, server: Optional[discord.Guild], allow_or_deny: str, *, command_name: str):
+        """Manages commands access for a server
 
+        If a server is not specified, the current server is used. The command name must not contain any aliases.
+        """
+        if server is None:
+            server = ctx.guild
+            if server is None:
+                raise commands.BadArgument('You must specify a server or use this command in a server.')
+        allow_or_deny = allow_or_deny.strip('"')
+        if allow_or_deny not in ('allow', 'deny'):
+            raise commands.BadArgument('Please enter either "allow" or "deny" before the command that you are changing the settings of.')
+        all_command_names = [x.name for x in self.bot.commands]
+        entered = command_name.split(' ')
+        for command_name in entered:
+            if command_name not in all_command_names:
+                raise commands.BadArgument(f'Command "{command_name}" not found.')
+
+        is_blacklist = allow_or_deny != 'allow'
         try:
             await self.bot.db.execute("""
                 INSERT INTO command_access_settings
                 (command_name, is_blacklist, object_type, object_ids)
-                VALUES ($1, FALSE, 'server', $2);
-                """, command_name, [server.id])
+                VALUES ($1, $2, 'server', $3);
+                """, command_name, is_blacklist, [server.id])
         except asyncpg.exceptions.UniqueViolationError:
             await self.bot.db.execute("""
                 UPDATE command_access_settings
                 SET object_ids = object_ids || $1
                 WHERE command_name = $2
-                    AND is_blacklist = FALSE
+                    AND is_blacklist = $3
                     AND object_type = 'server'
-                    AND $3 NOT IN object_ids;
-                """, [server.id], command_name, server.id)
+                    AND $1 != ANY(object_ids);
+                """, server.id, command_name, is_blacklist)
 
         await ctx.message.add_reaction('✅')
 
