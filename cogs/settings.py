aaee0b4bf691115fc02e8d251f2d32a7d17bbf6a
Chris Wheeler <39012918+wheelercj@users.noreply.github.com>
2021-07-24 20:13:51 +0000

    Save parent server and channel IDs of settings

--- cogs/settings.py
+++ cogs/settings.py
@@ -5,14 +5,16 @@ import asyncpg
 
 
 '''
-    CREATE TABLE IF NOT EXISTS command_access_settings (
+    CREATE TABLE command_access_settings (
         id SERIAL PRIMARY KEY,
         command_name TEXT,
         access TEXT CONSTRAINT valid_access CHECK (access = ANY('{"allow", "deny", "limit"}')),
+        parent_server_id BIGINT,  -- this is null if object_type is 'global' or 'server', and may be null if it's 'user'
+        parent_channel_id BIGINT,  -- this is null if object_type is anything but 'user', and may be null regardless
         object_type TEXT CONSTRAINT valid_type CHECK (object_type = ANY('{"global", "server", "role", "channel", "user"}')),
-        object_ids BIGINT[],  -- this is [null] if object_type is 'global'
-        UNIQUE (command_name, access, object_type)
-    )
+        object_ids BIGINT[],  -- this is [null] if and only if object_type is 'global'
+        UNIQUE (command_name, access, parent_server_id, parent_channel_id, object_type)
+    );
 '''
 # Each command may have multiple rows.
 
@@ -106,6 +108,10 @@ class Settings(commands.Cog):
             if r['object_type'] == 'global':
                 content += 'globally'
             else:
+                if r['parent_channel_id']:
+                    content += f'in channel {r["parent_channel_id"]}\n'
+                if r['parent_server_id']:
+                    content += f'in server {r["parent_server_id"]}\n'
                 content += 'by ' + r['object_type'] + 's:'
                 for ID in r['object_ids']:
                     content += f'\n {ID}'
@@ -122,7 +128,7 @@ class Settings(commands.Cog):
         For the `access` argument, you may enter "allow", "deny", or "limit". Limited access is the same as denied access, except that it allows exceptions. The command name must not contain any aliases.
         """
         await self.save_cmd_setting('global', None, access, command_name)
-        await ctx.send(f'New setting: {access} use of {command_name} globally.')
+        await ctx.send(f'New setting: {access} use of "{command_name}" globally.')
 
 
     @setting.command(name='server', aliases=['s'])
@@ -132,37 +138,47 @@ class Settings(commands.Cog):
         For the `access` argument, you may enter "allow", "deny", or "limit". Limited access is the same as denied access, except that it allows exceptions. The command name must not contain any aliases.
         """
         await self.save_cmd_setting('server', server.id, access, command_name)
-        await ctx.send(f'New setting: {access} use of {command_name} for server {server.name}.')
+        await ctx.send(f'New setting: {access} use of "{command_name}" for server {server.name}.')
 
 
     @setting.command(name='role', aliases=['r'])
     async def role_cmd_access(self, ctx, role: discord.Role, access: Access, *, command_name: CommandName):
-        """Manages commands access for a role
+        """Manages commands access for a role in this server
 
         For the `access` argument, you may enter "allow", "deny", or "limit". Limited access is the same as denied access, except that it allows exceptions. The command name must not contain any aliases.
         """
-        await self.save_cmd_setting('server', role.id, access, command_name)
-        await ctx.send(f'New setting: {access} use of {command_name} for server {role.name}.')
+        await self.save_cmd_setting('server', role.id, access, command_name, ctx.guild.id)
+        await ctx.send(f'New setting: {access} use of "{command_name}" for server {role.name}.')
 
 
     @setting.command(name='channel', aliases=['c'])
     async def channel_cmd_access(self, ctx, channel: Channel, access: Access, *, command_name: CommandName):
-        """Manages commands access for a channel
+        """Manages commands access for a channel in this server
 
         For the `access` argument, you may enter "allow", "deny", or "limit". Limited access is the same as denied access, except that it allows exceptions. The command name must not contain any aliases.
         """
-        await self.save_cmd_setting('channel', channel.id, access, command_name)
-        await ctx.send(f'New setting: {access} use of {command_name} for channel {channel.name}.')
+        await self.save_cmd_setting('channel', channel.id, access, command_name, ctx.guild.id)
+        await ctx.send(f'New setting: {access} use of "{command_name}" for channel {channel.name}.')
+
+
+    @setting.command(name='channel-member', aliases=['cm', 'channelmember'])
+    async def channel_member_cmd_access(self, ctx, channel: Channel, member: discord.Member, access: Access, *, command_name: CommandName):
+        """Manages commands access for a member for a channel in this server
+
+        For the `access` argument, you may enter "allow", "deny", or "limit". Limited access is the same as denied access, except that it allows exceptions. The command name must not contain any aliases.
+        """
+        await self.save_cmd_setting('user', member.id, access, command_name, ctx.guild.id, channel.id)
+        await ctx.send(f'New setting: {access} use of "{command_name}" for member {member.display_name} in channel {channel.name}.')
 
 
     @setting.command(name='member', aliases=['m'])
     async def member_cmd_access(self, ctx, member: discord.Member, access: Access, *, command_name: CommandName):
-        """Manages commands access for a member
+        """Manages commands access for a member of this server
 
         For the `access` argument, you may enter "allow", "deny", or "limit". Limited access is the same as denied access, except that it allows exceptions. The command name must not contain any aliases.
         """
-        await self.save_cmd_setting('user', member.id, access, command_name)
-        await ctx.send(f'New setting: {access} use of {command_name} for member {member.display_name}.')
+        await self.save_cmd_setting('user', member.id, access, command_name, ctx.guild.id)
+        await ctx.send(f'New setting: {access} use of "{command_name}" for member {member.display_name}.')
 
 
     @setting.command(name='user', aliases=['u'])
@@ -172,22 +188,24 @@ class Settings(commands.Cog):
         For the `access` argument, you may enter "allow", "deny", or "limit". Limited access is the same as denied access, except that it allows exceptions. The command name must not contain any aliases.
         """
         await self.save_cmd_setting('user', user.id, access, command_name)
-        await ctx.send(f'New setting: {access} use of {command_name} for user {user.name}#{user.discriminator}.')
+        await ctx.send(f'New setting: {access} use of "{command_name}" for user {user.name}#{user.discriminator}.')
 
 
-    async def save_cmd_setting(self, object_type: ObjectType, object_id: int, access: Access, command_name: CommandName) -> None:
+    async def save_cmd_setting(self, object_type: ObjectType, object_id: int, access: Access, command_name: CommandName, parent_server_id: int = None, parent_channel_id: int = None) -> None:
         """Saves a new command access setting to the database
         
-        object_id should be None if object_type is 'global'.
+        object_id should be None if object_type is 'global'. 
+        parent_server_id should be None if object_type is 'global' or 'server', and may be None if it's 'user'. 
+        parent_channel_id should be None if object_type is anything but 'user', and may be None regardless.
         """
         try:
             # Create a new row for this setting, but only if one does not
             # already exist for this type of setting.
             await self.bot.db.execute("""
                 INSERT INTO command_access_settings
-                (command_name, access, object_type, object_ids)
-                VALUES ($1, $2, $3, $4);
-                """, command_name, access, object_type, [object_id])
+                (command_name, access, object_type, object_ids, parent_server_id, parent_channel_id)
+                VALUES ($1, $2, $3, $4, $5, $6);
+                """, command_name, access, object_type, [object_id], parent_server_id, parent_channel_id)
         except asyncpg.exceptions.UniqueViolationError:
             # If a row for this type of setting already exists,
             # update the existing row.
@@ -197,8 +215,10 @@ class Settings(commands.Cog):
                 WHERE command_name = $2
                     AND access = $3
                     AND object_type = $4
+                    AND parent_server_id = $5
+                    AND parent_channel_id = $6
                     AND $1 != ANY(object_ids);
-                """, object_id, command_name, access, object_type)
+                """, object_id, command_name, access, object_type, parent_server_id, parent_channel_id)
 
 
 def setup(bot):
